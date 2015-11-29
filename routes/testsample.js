17e359c9fdfc6202d954f3452b1554de9fa3b532
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-11-29 02:04:08 -0500

    Basic Templates Fix

--- routes/testsample.js
+++ routes/testsample.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/testsample', function(req, res, next) {
+router.get('/', function(req, res, next) {
     console.log("got here");
     res.render('test');
 });
